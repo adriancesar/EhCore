05db7693e1e3db241e44f3669b3324b893888cd0
Lucas Didur <lucasdidur@hotmail.com>
2013-01-29 22:59:15 -0200

    Atualizado API para trade. Thx to Cybermaxke

---
+++ src/com/ehaqui/ehcore/api/trade/NMSMerchant.java
@@ -0,0 +1,116 @@
+package com.ehaqui.ehcore.api.trade;
+
+import net.minecraft.server.v1_4_R1.EntityHuman;
+import net.minecraft.server.v1_4_R1.EntityPlayer;
+import net.minecraft.server.v1_4_R1.IMerchant;
+import net.minecraft.server.v1_4_R1.MerchantRecipe;
+import net.minecraft.server.v1_4_R1.MerchantRecipeList;
+
+
+/**
+ * 
+ * This software is part of the MerchantAPI
+ * 
+ * @author Seppe Volkaerts (Cybermaxke)
+ * 
+ *         This api allows plugin developers to create on a easy way new
+ *         merchants they can open without villagers and adding as many
+ *         offers they want.
+ * 
+ *         MerchantAPI is free software: you can redistribute it and/or modify
+ *         it under the terms of the GNU General Public License as published by
+ *         the Free Software Foundation, either version 3 of the License, or
+ *         any later version.
+ * 
+ *         MerchantAPI is distributed in the hope that it will be useful,
+ *         but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *         MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *         GNU General Public License for more details.
+ * 
+ *         You should have received a copy of the GNU General Public License
+ *         along with MerchantAPI. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+public class NMSMerchant implements IMerchant
+{
+    private MerchantRecipeList o = new MerchantRecipeList();
+    private EntityHuman        c;
+    
+    /**
+     * Protected. (Only used by the api.)
+     */
+    protected NMSMerchant()
+    {
+        
+    }
+    
+    /**
+     * Adds a offer.
+     * 
+     * @param recipe
+     *            The offer.
+     */
+    @SuppressWarnings("unchecked")
+    @Override
+    public void a(MerchantRecipe recipe)
+    {
+        this.o.add(recipe);
+    }
+    
+    /**
+     * Set the customer.
+     * 
+     * @param player
+     *            The customer.
+     */
+    @Override
+    public void b_(EntityHuman player)
+    {
+        this.c = player;
+    }
+    
+    /**
+     * Returns all the offers.
+     * 
+     * @return The offers.
+     */
+    @Override
+    public MerchantRecipeList getOffers(EntityHuman player)
+    {
+        return this.o;
+    }
+    
+    /**
+     * Returns the customer.
+     * 
+     * @return The customer.
+     */
+    @Override
+    public EntityHuman m_()
+    {
+        return this.c;
+    }
+    
+    /**
+     * Sets the recipes with clearing the old ones.
+     * 
+     * @param recipes
+     *            The offers list.
+     */
+    public void setRecipes(MerchantRecipeList recipes)
+    {
+        this.o = recipes;
+    }
+    
+    /**
+     * Open the player gui.
+     * 
+     * @param player
+     *            The player which should open it.
+     */
+    public void openTrading(EntityPlayer player)
+    {
+        this.c = player;
+        this.c.openTrade(this);
+    }
+}
--- src/com/ehaqui/ehcore/api/trade/exception/NullTradeOfferException.java
+++
@@ -1,8 +0,0 @@
-package com.ehaqui.ehcore.api.trade.exception;
-
-public class NullTradeOfferException extends Exception
-{
-    
-    private static final long serialVersionUID = 1L;
-    
-}
